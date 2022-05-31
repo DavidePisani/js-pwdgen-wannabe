05af7568ce25d18a1893fb0553a3f1d26646f057
DavidePisani <davide.pisani96@gmail.com>
2022-05-31 13:29:29 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Chiedere all'utente nome cognome e colore preferito
+
+// nome utente
+const userName = prompt('Dimmi il tuo nome');
+    console.log(userName)
+
+// Cognome utente
+
+const userSurname = prompt('Dimmi il tuo Cognome');
+    console.log(userSurname)
