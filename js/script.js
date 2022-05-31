d1a03fc9d6624fe89ac191295e855742d546e5f6
DavidePisani <davide.pisani96@gmail.com>
2022-05-31 13:35:05 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Chiedere all'utente nome cognome e colore preferito
+// Chiedere all'utente nome cognome e colore preferito per creare password
 
 // nome utente
 const userName = prompt('Dimmi il tuo nome');
@@ -8,3 +8,17 @@ const userName = prompt('Dimmi il tuo nome');
 
 const userSurname = prompt('Dimmi il tuo Cognome');
     console.log(userSurname)
+
+// colore preferito utente
+
+const userColor = prompt('Dimmi il tuo colore preferito');
+    console.log(userColor)
+
+// Creazione password utente
+
+const userMessage = userName + userSurname + userColor + 21;
+console.log(userMessage)
+
+// Stampo password
+
+document.getElementById('result').innerHTML = userMessage
